69d385cbd97e3f9773a066198dea570d80e31fce
Jonathan Nguyen <wthunder@WTManjaro.Home>
2020-07-13 11:33:54 -0400

    Made some changes to the transparency

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Jonathan Nguyen"
+      user-mail-address "jnganguyen3@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -56,3 +56,6 @@
 ;; they are implemented.
 
 (setq doom-themes-treemacs-theme "doom-colors")
+
+(set-frame-parameter (selected-frame)'alpha '(90 . 90))
+(add-to-list 'default-frame-alist'(alpha . (90 . 90)))
